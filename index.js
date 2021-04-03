b22a7af7c9b2f623e32f111c15bf640a351b4f2e
Oleh Sulzhenko <webaleg@gmail.com>
2021-04-03 14:25:36 +0300

    server init

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const url = require('url');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const urlObj = url.parse(req.url, true);
+
+  switch (urlObj.pathname) {
+    case "/hello":
+      res.end(`Hello${urlObj?.query?.name ? `, ${urlObj.query.name}` : 'World'}`);
+      break
+    case "/goodbye":
+      res.end(`Goodbye${urlObj?.query?.name ? `, ${urlObj.query.name}` : ''}`);
+      break
+    default:
+      res.statusCode = 404;
+  }
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
