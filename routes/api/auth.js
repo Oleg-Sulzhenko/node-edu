f71de889c6f4e343f4fec145f31b0c7dee9f0f12
Oleh Sulzhenko <webaleg@gmail.com>
2021-09-17 19:16:50 +0300

    add Profile model, add auth middleware, add profile api

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const config = require('config');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+   try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+   } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+   }
+});
+
+// @route   POST api/auth
+// @desc    Athentificate User
+// @access  Public
+router.post('/', [
+  body('email', "Please include a valid email").isEmail(),
+  body('password', "Password is required").isLength({ min: 6 }),
+],
+async (req, res) => {
+  console.log('POST api/auth req.body :>> ', req.body);
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
+  const {email, password } = req.body;
+
+  try {
+    // Check if the User exist
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ errors: [{msg: 'Invalid credentials'}] });
+    }
+
+    // Check if password matches
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) {
+      return res.status(400).json({ errors: [{msg: 'Invalid credentials 2'}] });
+    }
+
+    // Return JWT
+    const payload = {user: {id: user.id}}
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      {
+        expiresIn: 360000
+      },
+      (err, token) => {
+        if(err) throw err;
+        res.json({ token });
+      }
+    ); 
+
+  } catch(err) {
+    console.log('err.message :>> ', err.message);
+    res.status(500).send('Server Error');
+  }
+  
+});
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,137 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { body, validationResult } = require('express-validator');
+
+// bring in normalize to give us a proper url, regardless of what user entered
+const normalize = require('normalize-url');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get current user profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id}).populate('user', ['name', 'avatar']);
+
+    if(!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+
+  }
+});
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+router.post('/', 
+  [auth, 
+    [
+      body('status', 'Status is required').not().isEmpty(),
+      body('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // destructure the request
+    const {
+      website,
+      skills,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+      // spread the rest of the fields we don't need to check
+      ...rest
+    } = req.body;
+
+    // build a profile
+    const profileFields = {
+      user: req.user.id,
+      website:
+        website && website !== ''
+          ? normalize(website, { forceHttps: true })
+          : '',
+      skills: Array.isArray(skills)
+        ? skills
+        : skills.split(',').map((skill) => ' ' + skill.trim()),
+      ...rest
+    };
+
+    // Build socialFields object
+    const socialFields = { youtube, twitter, instagram, linkedin, facebook };
+
+    // normalize social fields to ensure valid url
+    for (const [key, value] of Object.entries(socialFields)) {
+      if (value && value.length > 0)
+        socialFields[key] = normalize(value, { forceHttps: true });
+    }
+    // add to profileFields
+    profileFields.social = socialFields;
+
+    try {
+      // Using upsert option (creates new doc if no match is found):
+      let profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, upsert: true, setDefaultsOnInsert: true }
+      );
+      return res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server Error');
+    }
+
+  }
+);
 
 // @route   GET api/profile
-// @desc    Test route
+// @desc    Get all profiles
 // @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+
+    res.json(profiles);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get all profiles
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id});
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 
 module.exports = router;
